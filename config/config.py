804f1308c3aacd45134b8abfb230cb4cce4da114
Son3421 <son342121@gmail.com>
2024-06-04 13:51:03 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -42,7 +42,7 @@ SONG_DOWNLOAD_DURATION = int(
 
 # You'll need a Private Group ID for this.
 
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002193939214"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002178822934"))
 
 # A name for your Music bot.
 

ff3362057ceded242b5a18f6728896fd65c9030d
BaymoI <adamsnrobin@gmail.com>
2024-06-11 13:40:41 +0300

    Update helpers.py

--- strings/helpers.py
+++ strings/helpers.py
@@ -1,120 +1,115 @@
-#.
-
-HELP_1 = """✅**<u>BURDAKİ KOMUTLARI YALNIZCA GRUP ADMİNLERİ KULLANABİLİR:</u>**
-
-╰☞**Burdaki Komutları Normal Üyelerin De Kullanabilmesi İçin İstediğiniz Kişinin Mesajını Yanıtlayarak /yetkilendir Yazmanız Yeterlidir.**
-
-╰☞**/duraklat :** Müziği Duraklatır.
-╰☞**/devam :** Müziği Devam Ettirir.
-╰☞**/atla :** Çalan Müziği Atlar.
-╰☞**/dur veya /son :** Müziği Tamamen Durdurur.
-╰☞**/karistir :** Sıraya Aldığınız Parçaları Karışık Şekilde Oynatır.
-╰☞**/ileri :** Müziği İleri Sarar. (Örnek: /ilerisar 15 İle Çalan Müziği 15 Saniye İleri Alabilirsiniz.)
-╰☞**/geri:** Müziği Geri Sarar. (Örnek: /seekback 15 İle Çalan Müziği 15 Saniye Geri Alabilirsiniz.)
-╰☞**/restart :** Botu Yeniden Başlatır.
-
-✅<u>**Döngü Modu:**</u>
-╰☞**/tekrar :** Çalan Müziği Döngüye Alıp Tekrar Oynatır. 
-(Örnek: /tekrar 3 İle Seste Çalan Müziği 3 Kere Daha Oynatır.)"""
 
 
-HELP_2 = """✅<u>**OYNATMA KOMUTLARI:**</u>
+HELP_1 = """✨**<u>BURDAKİ KOMUTLARI YALNIZCA GRUP ADMİNLERİ KULLANABİLİR:</u>**
 
-╰☞**/oynat veya /play :** Müziği Oynatmaya Yarar.
-╰☞**-Örnek Kullanım:** /oynat şarkı İsmi // Veya Oynatmak İstediğiniz Şarkıyı Yanıtlayarak /oynat Yazabilirsiniz.
-(╰☞**Örnek:** /oynat uygar Doğanay )
+**Burdaki Komutları Normal Üyelerin De Kullanabilmesi İçin İstediğiniz Kişinin Mesajını Yanıtlayarak /yetkilendir Yazmanız Yeterlidir.**
 
-╰☞**/vplay :** Videoyu Seste Oynatmaya Yarar.
-╰☞**-Örnek Kullanım:** /vplay Video İsmi // Veya Oynatmak İstediğiniz Videoyu Yanıtlayarak /vplay Yazabilirsiniz.
+⏸️ **/duraklat :** Müziği Duraklatır.
+⏯️ **/devam :** Müziği Devam Ettirir.
+⏩ **/atla :** Çalan Müziği Atlar.
+⏸️ **/dur veya /son :** Müziği Tamamen Durdurur.
+🔀 **/karistir :** Sıraya Aldığınız Parçaları Karışık Şekilde Oynatır.
+⏭️ **/ilerisar :** Müziği İleri Sarar. (Örnek: /ilerisar 15 İle Çalan Müziği 15 Saniye İleri Alabilirsiniz.)
+⏮️ **/seekback :** Müziği Geri Sarar. (Örnek: /seekback 15 İle Çalan Müziği 15 Saniye Geri Alabilirsiniz.)
+🔁 **/restart :** Botu Yeniden Başlatır.
 
-╰☞**/indir [Müzik Adı] veya [Youtube Linki] :** Youtubedan İndirmek İstediğiniz Şarkıyı İndirebilirsiniz.
-(╰☞**Örnek:** /indir uygar doganay koptum )
+⮳<u>**Döngü Modu:**</u>
+🔂 **/dongu :** Çalan Müziği Döngüye Alıp Tekrar Oynatır. 
+(Örnek: /dongu 3 İle Seste Çalan Müziği 3 Kere Daha Oynatır.)"""
 
 
-✅**<u>Oynatma Listeleri:</u>**
-╰☞**/playlist :** Oynatma Listenizde Hangi Müziklerin Olduğuna Bakabilirsiniz.
-╰☞**/listemisil :** Oynatma Listenizden Silmek İstediğiniz Müziği Seçebilirsiniz.
-╰☞**/oynat Komutunu Tek Başına Kullanırsanız:** Oynatma Listeleri Ekranına Ulaşırsınız. """
+HELP_2 = """✨<u>**OYNATMA KOMUTLARI:**</u>
+
+▶️ **/oynat veya /play :** Müziği Oynatmaya Yarar.
+**-Örnek Kullanım:** /oynat şarkı İsmi // Veya Oynatmak İstediğiniz Şarkıyı Yanıtlayarak /oynat Yazabilirsiniz.
+(**Örnek:** /oynat Neşet Ertaş Gönül Dağı)
+
+🎦 **/vplay :** Videoyu Seste Oynatmaya Yarar.
+**-Örnek Kullanım:** /vplay Video İsmi // Veya Oynatmak İstediğiniz Videoyu Yanıtlayarak /vplay Yazabilirsiniz.
+
+📥 **/bul [Müzik Adı] veya [Youtube Linki] :** Youtubedan İndirmek İstediğiniz Şarkıyı İndirebilirsiniz.
+(**Örnek:** /indir Enercii)
 
 
-HELP_3 = """✅<u>**BOT KOMUTLARI**</u>
+✨**<u>Oynatma Listeleri:</u>**
+❤️‍🩹 **/playlist :** Oynatma Listenizde Hangi Müziklerin Olduğuna Bakabilirsiniz.
+🆑 **/listemisil :** Oynatma Listenizden Silmek İstediğiniz Müziği Seçebilirsiniz.
+📃 **/oynat Komutunu Tek Başına Kullanırsanız:** Oynatma Listeleri Ekranına Ulaşırsınız. """
 
-╰☞**/stat :** Bottaki Tüm İstatistikleri Görebilirsiniz. En Çok Müzik Oynatan Gruplar, Kullanıcılar, En Çok Oynatılan Müzikler Ve Daha Fazlası...
 
-╰☞**/sudolist :** Yardım İstemek İçin Bottaki Sudo Kullanıcılarına Ulaşabilirsiniz.
+HELP_3 = """✨<u>**BOT KOMUTLARI**</u>
 
-╰☞**/sarkisozu [Müzik Adı] :** Sözlerine Bakmak İstediğiniz Şarkıyı Arayabilirsiniz.
+🆘 **/sudolist :** Yardım İstemek İçin Bottaki Sudo Kullanıcılarına Ulaşabilirsiniz.
 
-╰☞**/indir [Müzik Adı] veya [Youtube Linki] :** Youtubedan İndirmek İstediğiniz Şarkıyı İndirebilirsiniz.
+🗨️ **/sarkisozu [Müzik Adı] :** Sözlerine Bakmak İstediğiniz Şarkıyı Arayabilirsiniz.
 
-╰☞**/videoindir [Müzik Adı] veya [Youtube Linki] :** Youtubedan İndirmek İstediğiniz Videoyu İndirebilirsiniz.
-(╰☞**Örnek:** /videoindir Neşet Ertaş Gönül Dağı)
+📥 **/bul [Müzik Adı] veya [Youtube Linki] :** Youtubedan İndirmek İstediğiniz Şarkıyı İndirebilirsiniz.
 
-╰☞**/sira :** Sırada Olan Müzikler Listesini Görebilirsiniz."""
+📑 **/sira :** Sırada Olan Müzikler Listesini Görebilirsiniz."""
 
-HELP_4 = """✅<u>**EKSTRA KOMUTLAR:**</u>
+HELP_4 = """✨<u>**EKSTRA KOMUTLAR:**</u>
 
-╰☞**/start :** Botun Başlatma Panelini Gösterir. 
+🌟 **/start :** Botun Başlatma Panelini Gösterir. 
 
-╰☞**/ayarlar :** Ayarlar Menüsüne Ulaşabilirsiniz.
+⚙️ **/ayarlar :** Ayarlar Menüsüne Ulaşabilirsiniz.
 
-╰☞**/yardim :** Botun Yardım Menüsüne Ulaşırsınız.
+🆘 **/yardim :** Botun Yardım Menüsüne Ulaşırsınız.
 
-╰☞**/ping :** Ping Süresini CPU Ve RAM Kullanımına Bakabilirsiniz."""
+📡 **/ping :** Ping Süresini CPU Ve RAM Kullanımına Bakabilirsiniz."""
 
 HELP_5 = """🔰**<u>BURASI ÇOK ÖNEMLİ DEĞİL, SADECE BOT YÖNETİCİLERİ İÇİN BİR REHBER:</u>**
 
-/addsudo [Username or Reply to a user]
-/delsudo [Username or Reply to a user]
+/addsudo [Kullanıcı adı veya kullanıcıya yanıt]
+/delsudo [Kullanıcı adı veya kullanıcıya yanıt]
 
 🤖**<u>BOT Komutları:</u>**
-/reboot - Reboot your Bot. 
-/update - Update Bot.
-/speedtest - Check server speeds
+/reboot - Botunuzu yeniden başlatın. 
+/update - Botu Güncelle.
+/speedtest - Sunucu hızlarını kontrol edin
 /maintenance [enable / disable] 
-/logger [enable / disable] - Bot logs the searched queries in logger group.
-/get_log [Number of Lines] - Get log of your bot from heroku or vps. Works for both.
-/autoend [enable|disable] - Enable Auto stream end after 3 mins if no one is listening.
+/logger [enable / disable] - Bot, aranan sorguları günlükçü grubuna kaydeder.
+/get_log [Satır Sayısı] - Botunuzun günlüğünü heroku veya vps'den alın. Her ikisi için de işe yarar.
+/autoend [enable|disable] - Kimse dinlemiyorsa 3 dakika sonra Otomatik akışı sonlandır özelliğini etkinleştirin.
 
-📈**<u>STATS COMMANDS:</u>**
-/aktifses - Check active voice chats on bot.
-/aktifvideo - Check active video calls on bot.
-/stat - Check Bots Stats
+📈**<u>İSTATİSTİK KOMUTLARI:</u>**
+/aktifses - Bottaki aktif sesli sohbetleri kontrol edin.
+/aktifvideo - Bottaki etkin video görüşmelerini kontrol edin.
+/stat - Bot İstatistiklerini Kontrol Edin
 
-⚠️**<u>BLACKLIST CHAT FUNCTION:</u>**
-/blacklistchat veya /engelle [CHAT_ID] - Blacklist any chat from using Music Bot
-/whitelistchat [CHAT_ID] - Whitelist any blacklisted chat from using Music Bot
-/blacklistedchat veya /engelliler- Check all blacklisted chats.
+⚠️**<u>KARA LİSTE SOHBET İŞLEVİ:</u>**
+/blacklistchat veya /engelle [CHAT_ID] - Music Bot'u kullanarak yapılan tüm sohbetleri kara listeye alın
+/whitelistchat [CHAT_ID] - Kara listeye alınmış herhangi bir sohbeti Music Bot kullanarak beyaz listeye alın
+/blacklistedchat veya /engelliler- Kara listeye alınan tüm sohbetleri kontrol edin.
 
-👤╰☞**<u>BLOCKED FUNCTION:</u>**
-/block [Username or Reply to a user] - Prevents a user from using bot commands.
-/unblock [Username or Reply to a user] - Remove a user from Bot's Blocked List.
-/blockedusers - Check blocked Users Lists
+👤**<u>ENGELLEME FONKSİYONU:</u>**
+/block [Kullanıcı adı veya kullanıcıya yanıt] - Kullanıcının bot komutlarını kullanmasını engeller.
+/unblock [Kullanıcı adı veya kullanıcıya yanıt] - Bir kullanıcıyı Bot'un Engellenen Listesinden kaldırma.
+/blockedusers - Engellenen Kullanıcı Listelerini kontrol edin
 
-👤╰☞**<u>GBAN FUNCTION:</u>**
-/gban [Username or Reply to a user] - Gban a user from bot's served chat and stop him from using your bot.
-/ungban [Username or Reply to a user] - Remove a user from Bot's gbanned List and allow him for using your bot
-/gbannedusers - Check Gbanned Users Lists
+👤**<u>GBAN FONKSİYONU:</u>**
+/gban [Kullanıcı adı veya kullanıcıya yanıt] - Bir kullanıcıyı bot sunucusu sohbetinden yasaklayın ve botunuzu kullanmasını engelleyin.
+/ungban [Kullanıcı adı veya kullanıcıya yanıt] - Bir kullanıcıyı Bot'un yasaklı listesinden kaldırın ve onun botunuzu kullanmasına izin verin
+/gbannedusers - G Yasaklı Kullanıcı Listelerini Kontrol Edin
 
-🎥**<u>VIDEOCALLS FUNCTION:</u>**
-/videolimit [Number of Chats] - Set a maximum Number of Chats allowed for Video Calls at a time. Default to 3 chats.
-/videomode [download|m3u8] - If download mode is enabled, Bot will download videos instead of playing them in M3u8 form. ByDefault to M3u8. You can use download mode when any query doesnt plays in m3u8 mode.
+🎥**<u>VİDEO FONKSİYONU:</u>**
+/videolimit [Sohbet Sayısı] - Aynı anda Video Görüşmeleri için izin verilen maksimum Sohbet Sayısını ayarlayın. Varsayılan olarak 3 sohbet.
+/videomode [download|m3u8] - İndirme modu etkinleştirilirse Bot, videoları M3u8 biçiminde oynatmak yerine indirecektir. Varsayılan olarak M3u8'e. M3u8 modunda herhangi bir sorgu oynatılmadığında indirme modunu kullanabilirsiniz.İndirme modu etkinleştirilirse Bot, videoları M3u8 biçiminde oynatmak yerine indirecektir. Varsayılan olarak M3u8'e. M3u8 modunda herhangi bir sorgu oynatılmadığında indirme modunu kullanabilirsiniz.
 
-⚡️**<u>PRIVATE BOT FUNCTION:</u>**
-/pro [CHAT_ID] - Allow a chat for using your bot.
-/unpro [CHAT_ID] - Disallow a chat from using your bot.
-/prolar - Check all allowed chats of your bot.
+⚡️**<u>ÖZEL BOT İŞLEVİ:</u>**
+/pro [CHAT_ID] - Botunuzu kullanmak için sohbete izin verin.
+/unpro [CHAT_ID] - Bir sohbetin botunuzu kullanmasına izin vermeyin.
+/prolar - Botunuzun izin verilen tüm sohbetlerini kontrol edin.
 
-🌐**<u>BROADCAST FUNCTION:</u>**
-/broadcast [Message or Reply to a Message] - Broadcast any message to Bot's Served Chats.
+🌐**<u>YAYIN FONKSİYONU:</u>**
+/broadcast [Mesaj Gönderme veya Mesaja Cevap Verme] - Herhangi bir mesajı Bot'un Sunulan Sohbetlerine yayınlayın.
 
-<u>Options for broadcast:</u>
-╰☞**-pin** : This will pin your message 
-╰☞**-pinloud** : This will pin your message with loud notification
-╰☞**-user** : This will broadcast your message to the users who have started your bot.
-╰☞**-assistant** : This will broadcast your message from assistant account of your bot.
-╰☞**-nobot** : This will force your bot to not broadcast message
+<u>Yayın seçenekleri:</u>
+**-pin** : Bu, mesajınızı sabitleyecektir 
+**-pinloud** : Bu, mesajınızı yüksek sesli bildirimle sabitleyecektir
+**-user** : Bu, mesajınızı botunuzu başlatan kullanıcılara yayınlayacaktır.
+**-assistant** : Bu, mesajınızı botunuzun asistan hesabından yayınlayacaktır.
+**-nobot** : Bu, botunuzu mesaj yayınlamamaya zorlayacaktır
 
-**Example:╰☞** `/broadcast -user -assistant -pin Hello Testing`
+**Örnek:** `/broadcast -user -assistant -pin Göktuğ`
 
 """
